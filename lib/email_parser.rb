2594a603936638da59008d6f6d0a134330fe9561
sam l <sluongo718@yahoo.com>
2020-09-09 04:19:58 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -15,7 +15,7 @@ class EmailAddressParser
   
   def parse
     puts splitEmail = email.split.collect {|email_name| email_name.split(",")}
-    email_ary = splitEmail.unique
+    email_ary = splitEmail.uniq!
     email_ary
   end
 end
